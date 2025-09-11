04aeaa40207dda823e950caaa62d6d0e55cab470
Axel Tapia <axurmen214@gmail.com>
2025-09-10 20:25:27 -0600

    Component Header

---
+++ src/app/pages/alert/alert.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alert',
+  templateUrl: './alert.page.html',
+  styleUrls: ['./alert.page.scss'],
+  standalone: false
+})
+export class AlertPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+  standalone: false
+})
+export class InicioPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
