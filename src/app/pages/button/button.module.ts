e0713c71c7bba0e31484e183b558a1402ff163ff
ZoeHerft <zoeherft@gmail.com>
2025-09-14 21:56:12 -0600

    feat: add ButtonPage con ejemplos de ion-button e iconos interactivos

---
+++ src/app/pages/button/button.module.ts
@@ -0,0 +1,11 @@
+﻿import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ButtonPage } from './button.page';
+
+@NgModule({
+  imports: [
+    RouterModule.forChild([{ path: '', component: ButtonPage }]),
+    ButtonPage
+  ]
+})
+export class ButtonPageModule {}
---
+++ src/app/pages/button/button.page.ts
@@ -0,0 +1,27 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.page.html',
+  styleUrls: ['./button.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+
+export class ButtonPage implements OnInit {
+
+  favorito: boolean = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick() {
+    this.favorito = !this.favorito;
+  }
+}
+
