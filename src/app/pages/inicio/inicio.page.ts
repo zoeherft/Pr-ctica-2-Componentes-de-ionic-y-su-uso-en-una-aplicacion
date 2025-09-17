2872c1ffb9072ac2da69426df975bbcba8e77865
ZoeHerft <zoeherft@gmail.com>
2025-09-17 07:25:00 -0600

    add: Cambios en el Menu

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
 
-interface Componente{
-  icon: string;
-  name: string;
-  redirectTo: string;
-}
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+
+import { DataService } from '../../services/data';              // <- ruta REAL del servicio
+import { Componente } from '../../interfaces/interfaces';       // <- interfaz
 
 @Component({
   selector: 'app-inicio',
@@ -13,83 +13,18 @@ interface Componente{
   standalone: false
 })
 export class InicioPage implements OnInit {
+  componentes!: Observable<Componente[]>;
 
-  components: Componente[] = [
-    {
-      icon: "american-football-outline",
-      name: 'Action Sheet',
-      redirectTo: '/action-sheet'
-    },
-    {
-      icon: "alert-circle-outline",
-      name: 'Alert',
-      redirectTo: '/alert'
-    },
-    {
-      icon: "beaker-outline",
-      name: 'Avatar',
-      redirectTo: '/avatar'
-    },
-    {
-      icon: "radio-button-off-outline",
-      name: 'Buttons',
-      redirectTo: '/button'
-    },
-    {
-      icon: "card-outline",
-      name: 'Cards',
-      redirectTo: '/card'
-    },
-    {
-      icon: "checkmark-circle-outline",
-      name: 'Checks',
-      redirectTo: '/check'
-    },
-    {
-      icon: "calendar-outline",
-      name: 'DateTime',
-      redirectTo: '/date-time'
-    },
-    {
-      icon: "car-outline",
-      name: 'Fab',
-      redirectTo: '/fab'
-    },
-    {
-      icon: "grid-outline",
-      name: 'Grid',
-      redirectTo: '/grid'
-    },
-    {
-      icon: "infinite-outline",
-      name: 'Infinite Scroll',
-      redirectTo: '/infinite'
-    },
-    {
-      icon: "hammer-outline",
-      name: 'Input',
-      redirectTo: '/input'
-    },
-    {
-      icon: "list-outline",
-      name: 'List - Sliding',
-      redirectTo: '/list'
-    },
-    {
-      icon: "reorder-three-outline",
-      name: 'List - Reorder',
-      redirectTo: '/list-reorder'
-    },
-    {
-      icon: "refresh-circle-outline",
-      name: 'Loading',
-      redirectTo: '/loading'
-    }
-  ];
+  constructor(
+    private menuCtrl: MenuController,
+    private dataService: DataService
+  ) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    this.componentes = this.dataService.getMenuOpts();
   }
 
+  mostrarMenu(): void {
+    this.menuCtrl.open('first');
+  }
 }
--- src/app/services/data.ts
+++ src/app/services/data.ts
@@ -1,15 +1,18 @@
+
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Componente } from '../interfaces/interfaces';         // <- IMPORTANTE
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class DataService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  // Asegúrate de que el nombre del método sea el correcto
   getUsuarios() {
     return this.http.get('https://jsonplaceholder.typicode.com/users');
+  }
+
+  getMenuOpts(): Observable<Componente[]> {
+    return this.http.get<Componente[]>('/assets/data/menu-opts.json'); // <- /assets/data/...
   }
 }
