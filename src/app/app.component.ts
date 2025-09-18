aae7ffada304808f1d393802e0ef7d5be4efc738
Coyeye <diana.coyotlc@alumno.buap.mx>
2025-09-18 11:20:40 -0600

    cambios de menu parte 2 bien hechos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { Componente } from './interfaces/interfaces';
+import { DataService } from './services/data';// <- ruta real data.service.ts
+
+import { SplashScreen } from '@capacitor/splash-screen';
+import { StatusBar, Style } from '@capacitor/status-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
-  standalone: false,
+  standalone: false // <- agrege esto por que en app.module.ts me generaba un error en declarations: [AppComponent]
 })
 export class AppComponent {
-  constructor() {}
+  componentes!: Observable<Componente[]>;
+
+  constructor(
+    private platform: Platform,
+    private dataService: DataService
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Ocultar splash y cambiar status bar
+      StatusBar.setStyle({ style: Style.Default });
+      SplashScreen.hide();
+      this.componentes = this.dataService.getMenuOpts();
+    });
+  }
 }
--- src/app/services/data.ts
+++ src/app/services/data.ts
@@ -1,4 +1,4 @@
-
+//data service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
